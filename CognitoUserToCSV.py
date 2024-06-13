84e38e1b52284a2aa6ddf0b64cbe3c25c06c51df
YazidHamdi <yazid.hamdi@outlook.com>
2024-06-13 17:38:35 +0200

    Enhance Cognito user export script to include user groups
    - Fetch and include all user groups in the CSV export.
    - Ensure pagination through all user records and groups.
    - Add 'Groups' to required export attributes.
    - Improve error handling and resource cleanup.

--- CognitoUserToCSV.py
+++ CognitoUserToCSV.py
@@ -40,51 +40,53 @@ if args.profile:
     PROFILE = args.profile
 if args.starting_token:
     STARTING_TOKEN = args.starting_token
-# print(1 if "email_verified" in REQUIRED_ATTRIBUTE else 0)
-# sys.exit()
 
 def datetimeconverter(o):
     if isinstance(o, datetime.datetime):
         return str(o)
 
-def get_list_cognito_users(cognito_idp_cliend, next_pagination_token ='', Limit = LIMIT):
-
-    return client.list_users(
-        UserPoolId = USER_POOL_ID,
-        #AttributesToGet = ['name'],
-        Limit = Limit,
-        PaginationToken = next_pagination_token
-    ) if next_pagination_token else client.list_users(
-        UserPoolId = USER_POOL_ID,
-        #AttributesToGet = ['name'],
-        Limit = Limit
+def get_list_cognito_users(cognito_idp_client, next_pagination_token='', Limit=LIMIT):
+    return cognito_idp_client.list_users(
+        UserPoolId=USER_POOL_ID,
+        Limit=Limit,
+        PaginationToken=next_pagination_token
+    ) if next_pagination_token else cognito_idp_client.list_users(
+        UserPoolId=USER_POOL_ID,
+        Limit=Limit
     )
 
-""" TODO: Write to file function helper for all Cognito Pool atrributes
-def write_cognito_records_to_file(file_name: str, cognito_records: list) -> bool:
-    try:
-        csv_file = open(file_name, 'a')
-        cognito_records.insert(0, ",".join(REQUIRED_ATTRIBUTE))
-        csv_file.writelines(cognito_records)
-        csv_file.close()
-        return True
-    except:
-        print("Something went wrong while writing to file")
-"""
+def get_user_groups(cognito_idp_client, username):
+    groups = []
+    next_token = None
+    while True:
+        response = cognito_idp_client.admin_list_groups_for_user(
+            UserPoolId=USER_POOL_ID,
+            Username=username,
+            NextToken=next_token
+        ) if next_token else cognito_idp_client.admin_list_groups_for_user(
+            UserPoolId=USER_POOL_ID,
+            Username=username
+        )
+        groups.extend([group['GroupName'] for group in response['Groups']])
+        next_token = response.get('NextToken')
+        if not next_token:
+            break
+    return ','.join(groups)
 
+# Initialize the boto3 client with the correct profile if provided
 if PROFILE:
     session = boto3.Session(profile_name=PROFILE)
-    client = session.client('cognito-idp', REGION)
+    client = session.client('cognito-idp', region_name=REGION)
 else:
-    client = boto3.client('cognito-idp', REGION)
+    client = boto3.client('cognito-idp', region_name=REGION)
 
+REQUIRED_ATTRIBUTE.append('Groups')
 csv_new_line = {REQUIRED_ATTRIBUTE[i]: '' for i in range(len(REQUIRED_ATTRIBUTE))}
 try:
-    csv_file = open(CSV_FILE_NAME, 'w' ,encoding="utf-8")
+    csv_file = open(CSV_FILE_NAME, 'w', encoding="utf-8")
     csv_file.write(",".join(csv_new_line.keys()) + '\n')
 except Exception as err:
-    #status = err.response["ResponseMetadata"]["HTTPStatusCode"]
-    error_message = repr(err)#err.strerror
+    error_message = repr(err)
     print(Fore.RED + "\nERROR: Can not create file: " + CSV_FILE_NAME)
     print("\tError Reason: " + error_message)
     exit()
@@ -97,37 +99,32 @@ while pagination_token is not None:
     csv_lines = []
     try:
         user_records = get_list_cognito_users(
-            cognito_idp_cliend = client,
-            next_pagination_token = pagination_token,
-            Limit = LIMIT if LIMIT < MAX_NUMBER_RECORDS else MAX_NUMBER_RECORDS
+            cognito_idp_client=client,
+            next_pagination_token=pagination_token,
+            Limit=LIMIT if LIMIT < MAX_NUMBER_RECORDS else MAX_NUMBER_RECORDS
         )
     except client.exceptions.ClientError as err:
-        #status = err.response["ResponseMetadata"]["HTTPStatusCode"]
         error_message = err.response["Error"]["Message"]
         print(Fore.RED + "Please Check your Cognito User Pool configs")
         print("Error Reason: " + error_message)
         csv_file.close()
         exit()
-    except:
-        print(Fore.RED + "Something else went wrong")
+    except Exception as e:
+        print(Fore.RED + "Something else went wrong: ", str(e))
         csv_file.close()
         exit()
 
-    # json_formatted_str = json.dumps(user_records, indent=4, default=datetimeconverter)
-    # print(json_formatted_str)
-
-    """ Check if there next paginatioon is exist """
-    if set(["PaginationToken","NextToken"]).intersection(set(user_records)):
+    if set(["PaginationToken", "NextToken"]).intersection(set(user_records)):
         pagination_token = user_records['PaginationToken'] if "PaginationToken" in user_records else user_records['NextToken']
     else:
         pagination_token = None
-    # json_formatted_str = json.dumps(user_records, indent=4, default=datetimeconverter)
-    # print(json_formatted_str)
 
     for user in user_records['Users']:
-        """ Fetch Required Attributes Provided """
         csv_line = csv_new_line.copy()
         for requ_attr in REQUIRED_ATTRIBUTE:
+            if requ_attr == 'Groups':
+                csv_line[requ_attr] = get_user_groups(client, user['Username'])
+                continue
             csv_line[requ_attr] = ''
             if requ_attr in user.keys():
                 csv_line[requ_attr] = str(user[requ_attr])
@@ -140,23 +137,17 @@ while pagination_token is not None:
 
     csv_file.writelines(csv_lines)
 
-    """ Display Proccess Infor """
     pagination_counter += 1
     exported_records_counter += len(csv_lines)
     print(Fore.YELLOW + "Page: #{} \n Total Exported Records: #{} \n".format(str(pagination_counter), str(exported_records_counter)))
-    # print("Pagination Token: \n{}\n".format(pagination_token))
 
     if MAX_NUMBER_RECORDS and exported_records_counter >= MAX_NUMBER_RECORDS:
         print(Fore.GREEN + "INFO: Max Number of Exported Reached")
         break
 
     if pagination_token is None:
-        #json_formatted_str = json.dumps(user_records, indent=4, default=datetimeconverter)
-        #print(json_formatted_str)
         print(Fore.GREEN + "INFO: End of Cognito User Pool reached")
 
-    """ Cool Down before next batch of Cognito Users """
     time.sleep(0.15)
 
-""" Close File """
 csv_file.close()
